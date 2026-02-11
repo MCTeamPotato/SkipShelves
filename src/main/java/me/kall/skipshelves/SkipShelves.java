bbbf4e345408054d7e08a3aa945ab1604a634552
狂想ThePotato <110756250+Kasualix@users.noreply.github.com>
2026-02-11 22:30:01 +0800

    1165 port

--- src/main/java/me/kall/skipshelves/SkipShelves.java
+++ src/main/java/me/kall/skipshelves/SkipShelves.java
@@ -1,26 +1,25 @@
 package me.kall.skipshelves;
 
 import net.minecraftforge.common.ForgeConfigSpec;
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import org.jetbrains.annotations.NotNull;
 
 @Mod(SkipShelves.MOD_ID)
 public final class SkipShelves {
     public static final String MOD_ID = "skipshelves";
 
-    public SkipShelves(@NotNull FMLJavaModLoadingContext context) {
-        context.registerConfig(ModConfig.Type.COMMON, CONFIG);
+    public SkipShelves() {
+        ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, CONFIG);
     }
 
     public static final ForgeConfigSpec CONFIG;
-    public static final ForgeConfigSpec.DoubleValue POWER;
+    public static final ForgeConfigSpec.IntValue POWER;
 
     static {
         ForgeConfigSpec.Builder builder = new ForgeConfigSpec.Builder();
         builder.push("SkipShelves");
-        POWER = builder.defineInRange("EnchantmentTablePower", 15.0D, 0.0D, Double.MAX_VALUE);
+        POWER = builder.defineInRange("EnchantmentTablePower", 15, 0, Integer.MAX_VALUE);
         builder.pop();
         CONFIG = builder.build();
     }
--- src/main/java/me/kall/skipshelves/mixin/EnchantmentMenuMixin.java
+++ src/main/java/me/kall/skipshelves/mixin/EnchantmentMenuMixin.java
@@ -6,22 +6,19 @@ import net.minecraft.world.inventory.EnchantmentMenu;
 import net.minecraft.world.level.Level;
 import org.spongepowered.asm.mixin.Dynamic;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Constant;
-import org.spongepowered.asm.mixin.injection.ModifyConstant;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.*;
 
 @Mixin(EnchantmentMenu.class)
 public abstract class EnchantmentMenuMixin {
     @Dynamic
-    @Redirect(method = {"method_17411", "lambda$slotsChanged$0", "m_39483_"}, at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/block/EnchantmentTableBlock;isValidBookShelf(Lnet/minecraft/world/level/Level;Lnet/minecraft/core/BlockPos;Lnet/minecraft/core/BlockPos;)Z"), require = 0)
-    private boolean onEnchant(Level level, BlockPos tablePos, BlockPos offsetPos) {
-        return false;
+    @ModifyConstant(method = {"method_17411", "lambda$slotsChanged$0", "func_217002_a"}, constant = @Constant(intValue = 0, ordinal = 0))
+    private int modifyPower(int origin) {
+        return SkipShelves.POWER.get();
     }
 
     @Dynamic
-    @ModifyConstant(method = {"method_17411", "lambda$slotsChanged$0", "m_39483_"}, constant = @Constant(floatValue = 0.0F))
-    private float modifyPower(float origin) {
-        return SkipShelves.POWER.get().floatValue();
+    @Redirect(method = {"method_17411", "lambda$slotsChanged$0", "func_217002_a"}, at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/Level;isEmptyBlock(Lnet/minecraft/core/BlockPos;)Z", ordinal = 0))
+    private boolean skip(Level instance, BlockPos pos) {
+        return false;
     }
 }
